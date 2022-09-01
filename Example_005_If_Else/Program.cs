3b19029f8347d22e0a2df7d58e1234fb7b7f859c
Natalia <kotovana_92@mail.ru>
2022-09-01 15:25:18 +0800

    Внесли корректировки в первом проекте и добавили пятный проект

---
+++ Example_005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
